cb0bdeccab9dd1d2df3b13e266e00a39eb799f9d
RodolfoCamposGlz <rodolfocampos459@gmail.com>
2019-11-24 14:56:16 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import {showAlter} from './messages.js';
+
+import "./styles.scss";
+
+document.getElementById('btn-alert').addEventListener('click', showAlter);
---
+++ src/messages.js
@@ -0,0 +1,3 @@
+export function showAlter(){
+    alert('hello world')
+}
